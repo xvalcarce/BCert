5a5593d003ee58ef4c95ee4ff7dc6a02964bca63
Xavier Valcarce <xvalcarce@protonmail.ch>
2020-08-14 17:15:55 +0200

    Add maximization test

---
+++ example/maximization_single.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os, sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import bcert as bc
+import numpy as np
+
+class MaxFunction(bc.FunLipschitz):
+    def __init__(self, dim=2):
+        super().__init__(dim)
+    def f(self, x):
+        return np.cos(x[0])+np.sin(x[1])
+    def bound_grad(self):
+        return np.sqrt(2)
+
+def find_maximum():
+    print("Finding the maximum of the function f(x0,x1)=cos(x0)+sin(x1) for (x0,x1) in [-pi,pi]^2")
+    fun = MaxFunction()
+    space = bc.CompactSpace(np.array([[-np.pi,np.pi]]*2))
+    step = np.pi/4
+    tol = 1e-1
+    res = bc.maxCert(fun, space, step, guess=2, tol=tol)
--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import unittest
 from example.bcert_simple import certification as simple_cert
 from example.bcert_parallelized import certification as par_cert
 from example.bcert_user_parallelized import certification as usr_par_cert
+from example.maximization_single import find_maximum as find_max
 
 class TestSingleThread(unittest.TestCase):
     def test_bc_single_thread(self):
@@ -25,6 +26,14 @@ class TestMultiThread(unittest.TestCase):
             usr_par_cert()
         except:
             self.fail("Fail to run boundCertUserPar (mutlithread-thread, usr generated.)")
+
+class TestOptimization(unittest.TestCase):
+    def test_find_max(self):
+        try:
+            print("\n")
+            find_max()
+        except:
+            self.fail("Maximization on single thread failed.")
 
 if __name__ == '__main__':
     unittest.main()
