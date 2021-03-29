f7b31db097861108eb9b8f7a2c7408b1cdbb0a20
Xavier Valcarce <xvalcarce@protonmail.ch>
2021-03-29 11:36:53 +0200

    Correct verbose msg

--- example/maximization_single.py
+++ example/maximization_single.py
@@ -16,7 +16,7 @@ class MaxFunction(bc.FunLipschitz):
         return np.sqrt(2)
 
 def find_maximum():
-    print("Finding the maximum of the function f(x0,x1)=cos(x0)+sin(x1) for (x0,x1) in [-pi,pi]^2")
+    print("Finding an upper bound on the maximum of the function f(x0,x1)=cos(x0)+sin(x1) for (x0,x1) in [-pi,pi]^2")
     fun = MaxFunction()
     space = bc.CompactSpace(np.array([[-np.pi,np.pi]]*2))
     step = np.pi/4
